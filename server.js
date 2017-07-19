e49b2ad0b46940068044cd58714720449728ad21
heroku-kensho <xhayashi.mikihiro2@contract.isid.co.jp>
2017-07-19 10:39:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,39 @@ app.post('/update', function(req, res) {
     });
 });
 
+app.post('/updateTest', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        // attempt the phone number of a salesforce contact
+        conn.query(
+            '$1',
+            [req.body.sql.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query('INSERT INTO salesforce.Contact (Phone, MobilePhone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
+                  [req.body.phone.trim(), req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
+                  function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+        );
+    });
+});
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
